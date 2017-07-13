1b227f2a2c0b3c18a80196405bb84061f3eec04e
Brad Goldsmith <b-rad@Brads-MBP.fios-router.home>
2017-07-12 23:11:49 -0400

    slightly more styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,7 +41,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
-                                    <label>
+                                    <label class="register">
                                         <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
                                     </label>
                                 </div>
