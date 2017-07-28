95a000df0251cef30fc6d04ac9a0ed5de7bf1ad7
Brad Goldsmith <b-rad@Brads-MBP.fios-router.home>
2017-07-28 16:49:40 -0400

    this is bullshit

--- resources/views/user/editwod.blade.php
+++ resources/views/user/editwod.blade.php
@@ -10,7 +10,7 @@
 
   {{ csrf_field() }}
   {{ method_field('PUT') }}
-
+<!--
   <div class="form-group row">
     <label for="board_img" class="col-sm-2 form-control-label">Picture of your WOD (whiteboard)</label>
     <div class="col-sm-6">
@@ -19,6 +19,7 @@
              name="board_img">
     </div>
   </div>
+-->
 
 
 
