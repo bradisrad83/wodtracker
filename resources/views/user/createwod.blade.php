09bff82a2af09bcf43c6df2b0bd8d8ce46d39f63
Brad Goldsmith <b-rad@Brads-MBP.fios-router.home>
2017-07-29 12:58:57 -0400

    working on resizing again

--- resources/views/user/createwod.blade.php
+++ resources/views/user/createwod.blade.php
@@ -10,7 +10,7 @@
 
   {{ csrf_field() }}
 
-  <!--
+
  <div class="form-group row">
     <label for="board_img" class="col-sm-2 form-control-label">Picture of your WOD (whiteboard)</label>
     <div class="col-sm-6">
@@ -19,7 +19,7 @@
              name="board_img">
     </div>
   </div>
--->
+
 
   <div class="form-group row">
     <label for="strength" class="col-sm-2 form-control-label">Strength Portion</label>
