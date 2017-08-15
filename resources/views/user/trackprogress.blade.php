b0ca215488ff427ae268dabe495f0621ba3c96f2
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2017-08-15 12:50:47 -0400

    moved the modals into the foreach loop so all pop up not just the last one

--- resources/views/user/trackprogress.blade.php
+++ resources/views/user/trackprogress.blade.php
@@ -21,6 +21,30 @@
             </button>
           </div>
         @endif
+        <!-- Modal -->
+        <div class="modal fade" id="myModal{{$values->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                @if($values->wod_date)
+                  <h4 class="modal-title" id="myModalLabel">{{$values->wod_date}}</h4>
+                @else
+                  <h4 class="modal-title" id="myModalLabel">{{$values->created_at->format('M d Y')}}</h4>
+                @endif
+              </div>
+              <div class="modal-body">
+                @if($values->wod_img)
+                  <img src="https://s3.us-east-2.amazonaws.com/dwtbucket/{{$values->wod_img}}" alt = "" height="150" width="300">
+                @endif
+                <p class="mod"><strong>Strength: </strong>{{$values->strength}}</p>
+                <p class="mod"><strong>Strength Notes: </strong>{{$values->strength_notes}}</p>
+              </div>
+              <div class="modal-footer">
+                    <button type="button" class="btn btn-default navbar-inverse" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
       @endforeach
     @endif
   @else
@@ -33,41 +57,36 @@
             {{$values->created_at->format('M d Y')}}
           </button>
         </div>
+        <!-- Modal -->
+        <div class="modal fade" id="myModal{{$values->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                @if($values->wod_date)
+                  <h4 class="modal-title" id="myModalLabel">{{$values->wod_date}}</h4>
+                @else
+                  <h4 class="modal-title" id="myModalLabel">{{$values->created_at->format('M d Y')}}</h4>
+                @endif
+              </div>
+              <div class="modal-body">
+                @if($values->wod_img)
+                  <img src="https://s3.us-east-2.amazonaws.com/dwtbucket/{{$values->wod_img}}" alt = "" height="150" width="300">
+                @endif
+                <p class="mod"><strong>WOD Type:</strong> {{$values->wod_type}}</p>
+                <p class="mod"><strong>WOD: </strong>{{$values->wod}}</p>
+                <p class="mod"><strong>WOD Results:</strong> {{$values->wod_results}}</p>
+                <p class="mod"><strong>WOD Notes:</strong> {{$values->wod_notes}}</p>
+              </div>
+              <div class="modal-footer">
+                    <button type="button" class="btn btn-default navbar-inverse" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
       @endforeach
     @endif
   @endif
-
-  <!-- Modal -->
-  <div class="modal fade" id="myModal{{$values->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          @if($values->wod_date)
-            <h4 class="modal-title" id="myModalLabel">{{$values->wod_date}}</h4>
-          @else
-            <h4 class="modal-title" id="myModalLabel">{{$values->created_at->format('M d Y')}}</h4>
-          @endif
-        </div>
-        <div class="modal-body">
-          @if($values->wod_img)
-            <img src="https://s3.us-east-2.amazonaws.com/dwtbucket/{{$values->wod_img}}" alt = "" height="150" width="300">
-          @endif
-          @if($tracker=='strength')
-            <p class="mod"><strong>Strength: </strong>{{$values->strength}}</p>
-            <p class="mod"><strong>Strength Notes: </strong>{{$values->strength_notes}}</p>
-          @else
-            <p class="mod"><strong>WOD Type:</strong> {{$values->wod_type}}</p>
-            <p class="mod"><strong>WOD: </strong>{{$values->wod}}</p>
-            <p class="mod"><strong>WOD Results:</strong> {{$values->wod_results}}</p>
-            <p class="mod"><strong>WOD Notes:</strong> {{$values->wod_notes}}</p>
-          @endif
-        </div>
-        <div class="modal-footer">
-              <button type="button" class="btn btn-default navbar-inverse" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-    </div>
-  </div>
 
 
 @stop
