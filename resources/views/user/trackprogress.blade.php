410b859f51afebb4fff0206940611c3ca9bda010
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2017-08-15 10:04:26 -0400

    changed modal structure

--- resources/views/user/trackprogress.blade.php
+++ resources/views/user/trackprogress.blade.php
@@ -21,30 +21,6 @@
             </button>
           </div>
         @endif
-      <!-- Modal -->
-      <div class="modal fade" id="myModal{{$values->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              @if($values->wod_date)
-                <h4 class="modal-title" id="myModalLabel">{{$values->wod_date}}</h4>
-              @else
-                <h4 class="modal-title" id="myModalLabel">{{$values->created_at->format('M d Y')}}</h4>
-              @endif
-            </div>
-            <div class="modal-body">
-              @if($values->wod_img)
-                <img src="https://s3.us-east-2.amazonaws.com/dwtbucket/{{$values->wod_img}}" alt = "" height="150" width="300">
-              @endif
-              <p class="mod"><strong>Strength: </strong>{{$values->strength}}</p>
-              <p class="mod"><strong>Strength Notes: </strong>{{$values->strength_notes}}</p>
-            </div>
-            <div class="modal-footer">
-                  <button type="button" class="btn btn-default navbar-inverse" data-dismiss="modal">Close</button>
-            </div>
-          </div>
-        </div>
-      </div>
       @endforeach
     @endif
   @else
@@ -57,32 +33,34 @@
             {{$values->created_at->format('M d Y')}}
           </button>
         </div>
-        <!-- Modal -->
-        <div class="modal fade" id="myModal{{$values->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">{{$values->created_at->format('M d Y')}}</h4>
-              </div>
-              <div class="modal-body">
-                @if($values->wod_img)
-                  <img src="https://s3.us-east-2.amazonaws.com/dwtbucket/{{$value->wod_img}}" alt = "" height="150" width="300">
-                @endif
-                <p class="mod"><strong>Type of WOD: </strong>{{$values->wod_type}}</p>
-                <p class="mod"><strong>WOD: </strong>{{$values->wod}}</p>
-                <p class="mod"><strong>WOD Results: </strong>{{$values->wod_results}}</p>
-                <p class="mod"><strong>WOD Notes: </strong>{{$values->wod_notes}}</p>
-              </div>
-              <div class="modal-footer">
-                    <button type="button" class="btn btn-default navbar-inverse" data-dismiss="modal">Close</button>
-              </div>
-            </div>
-          </div>
-        </div>
       @endforeach
     @endif
   @endif
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal{{$values->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          @if($values->wod_date)
+            <h4 class="modal-title" id="myModalLabel">{{$values->wod_date}}</h4>
+          @else
+            <h4 class="modal-title" id="myModalLabel">{{$values->created_at->format('M d Y')}}</h4>
+          @endif
+        </div>
+        <div class="modal-body">
+          @if($values->wod_img)
+            <img src="https://s3.us-east-2.amazonaws.com/dwtbucket/{{$values->wod_img}}" alt = "" height="150" width="300">
+          @endif
+          <p class="mod"><strong>Strength: </strong>{{$values->strength}}</p>
+          <p class="mod"><strong>Strength Notes: </strong>{{$values->strength_notes}}</p>
+        </div>
+        <div class="modal-footer">
+              <button type="button" class="btn btn-default navbar-inverse" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
 @stop
