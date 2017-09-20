d34977700e89d2afb9f0e5677b69df3933f491a7
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2017-09-20 12:41:15 -0400

    changed the tracking to a search bar for more possibilities

--- resources/views/user/progress.blade.php
+++ resources/views/user/progress.blade.php
@@ -3,26 +3,23 @@
 @section('content')
 <div class = "container">
   <h1>Track Progress</h1>
-  <h5>*please not that if you have been taking pictures of WODs this feature will not work*</h5>
+  <h5>*****please not that if you have been taking pictures of WODs this feature will not work*</h5>
     <form action="/progress/user/{{$user->id}}" method="POST">
         {{ csrf_field() }}
         <h3>What movement would you like to track?</h3>
-          <div class="row">
-            <div class="col-sm-5">
-              <select id="movement" name="movement" class="form-control">
-                <option>Clean</option>
-                <option>Deadlift</option>
-                <option>Jerk</option>
-                <option>Press</option>
-                <option>Squat</option>
-                <option>Snatch</option>
-                <option>Thruster</option>
-              </select>
+          <div class="form-group row">
+            <label for="movement"></label>
+            <div class="col-sm-6">
+              <input class="form-control"
+                    type="text"
+                    name="movement"
+                    id="movement"
+                    placeholder="thruster, pull ups, T2B, deadlift etc...">
             </div>
           </div>
           <br>
          <h3>Where would you want to track it? </h3>
-          <div class="row">
+          <div class="form-group row">
             <div class="col-sm-5">
               <select id="tracker" name="track" class="form-control">
                 <option>strength</option>
@@ -38,6 +35,11 @@
           </div>
 
       </form>
+      <h5>*****If you entered MU for muscle ups, and search for muscle ups, you will not get any results back.
+          If you entered kettle bell swings and search for KBS, then you will not get any results back.  
+          Please make sure to enter your seach term exactly how you entered them into the WOD.</h5>
+          
+
 
 
 @stop
